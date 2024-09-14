06363d4795d974f9790bfb1467e24b7bc0a8fc0f
pooja <poojaviswakarma687@gmail.com>
2024-09-14 23:56:37 +0530

    custom-react-library

---
+++ customReact.js
@@ -0,0 +1,62 @@
+
+const userName = 'hey this is pooja '
+const reactElement = {
+    //take three keys
+    // type-which tag 
+    type: 'a',
+    // props-attribute
+    props: {
+        href: 'https://google.com',
+        target: '_blank'
+    },
+    // html-text
+    children: `click to visit google wwwab,${userName}`
+    // Note-{userName}-varible can't take like {if(true)userName}-bcoz it is initialized inside object ,like reactElement in react 
+    // this is ,how a variable append in dom 
+
+
+}
+
+// function customRender(reactElement, container) {
+//     //1.need to create domElement
+//     const domElement = document.createElement(reactElement.type)
+//     // 2.assign text-innnerHTLM
+
+//     domElement.innerHTML = reactElement.children
+//     //3.setAttribute
+
+//     domElement.setAttribute("href", reactElement.props.href)
+//     domElement.setAttribute("target", reactElement.props.target)
+
+//     // 4.need to append this domElement in container -root
+//     container.appendChild(domElement)
+
+// }
+
+// but manually setting attribute is not possible
+
+function customRender(reactElement, container) {
+    //1.need to create domElement
+    const domElement = document.createElement(reactElement.type)
+    // 2.assign text-innnerHTLM
+
+    domElement.innerHTML = reactElement.children
+    //3.setAttribute
+
+    for (let prop in reactElement.props){
+        domElement.setAttribute(prop,reactElement.props[prop])
+    }
+
+    // 4.need to append this domElement in container -root
+    container.appendChild(domElement)
+
+}
+
+// creating root element  
+const mainContainer = document.querySelector("#root")
+
+//custom render method for render root elemet 
+
+customRender(reactElement, mainContainer)
+
+
---
+++ readme.md
@@ -0,0 +1,10 @@
+<!-- create a project -->
+<!-- index.html -->
+<!-- customReact.js -->
+<!-- inside js ,need to create dom -using createRoot method  -->
+
+<!-- domElement-object {type:'',props:{target:'',href:'',...},children:"text"} -->
+
+<!-- need to create domElewment-1.creatweElement,innerHTML,setAttribute,appewndChild -->
+<!-- //neewd to rewnder customRender(reactElement,container(means-root))  -->
+
